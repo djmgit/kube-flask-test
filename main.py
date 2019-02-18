adabcbeffd22dafd3820d8a886e94fa1d65bb0bf
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-02-18 10:34:20 +0530

    Add soure

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, request, jsonify, render_template
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "THIS IS SECRET"
+
+branch = "master"
+
+@app.route("/app")
+def index():
+	return 'Hellow world! I am on branch {}'.format(branch)
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=7070)
+
